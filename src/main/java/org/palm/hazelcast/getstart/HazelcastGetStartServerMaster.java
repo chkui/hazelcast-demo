6804b3ec13c81e590e73d52b2fb79dcc484fbe38
chkui <chenkui@chkui.com>
2016-08-06 21:24:58 +0800

    first local commit

---
+++ src/main/java/org/palm/hazelcast/getstart/HazelcastGetStartServerMaster.java
@@ -0,0 +1,22 @@
+package org.palm.hazelcast.getstart;
+
+import java.util.Map;
+import java.util.Queue;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class HazelcastGetStartServerMaster {
+	public static void main(String[] args) {
+		// 创建一个 hazelcastInstance实例
+		HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+		// 创建集群Map
+		Map<Integer, String> clusterMap = instance.getMap("MyMap");
+		clusterMap.put(1, "Hello hazelcast map!");
+
+		// 创建集群Queue
+		Queue<String> clusterQueue = instance.getQueue("MyQueue");
+		clusterQueue.offer("Hello hazelcast!");
+		clusterQueue.offer("Hello hazelcast queue!");
+	}
+}
---
+++ src/main/java/org/palm/hazelcast/getstart/HazelcastGetStartServerSlave.java
@@ -0,0 +1,22 @@
+package org.palm.hazelcast.getstart;
+
+import java.util.Map;
+import java.util.Queue;
+
+import com.hazelcast.core.Hazelcast;
+import com.hazelcast.core.HazelcastInstance;
+
+public class HazelcastGetStartServerSlave {
+	public static void main(String[] args) {
+		//创建一个 hazelcastInstance实例
+		HazelcastInstance instance = Hazelcast.newHazelcastInstance();
+		Map<Integer, String> clusterMap = instance.getMap("MyMap");
+		Queue<String> clusterQueue = instance.getQueue("MyQueue");
+		
+		System.out.println("Map Value:" + clusterMap.get(1));
+		System.out.println("Queue Size :" + clusterQueue.size());
+		System.out.println("Queue Value 1:" + clusterQueue.poll());
+		System.out.println("Queue Value 2:" + clusterQueue.poll());
+		System.out.println("Queue Size :" + clusterQueue.size());
+	}
+}
