3121c3b5062547c222339dffdd8d4fb31349ce8b
chkui <chenkui@chkui.com>
2016-08-11 23:23:23 +0800

    modify HazelcastConfigRuntimeModify. add more entrty modify code

--- src/main/java/org/palm/hazelcast/config/HazelcastConfigRuntimeModify.java
+++ src/main/java/org/palm/hazelcast/config/HazelcastConfigRuntimeModify.java
@@ -1,6 +1,8 @@
 package org.palm.hazelcast.config;
 
 import com.hazelcast.config.Config;
+import com.hazelcast.config.JoinConfig;
+import com.hazelcast.config.MulticastConfig;
 import com.hazelcast.config.NetworkConfig;
 import com.hazelcast.core.Hazelcast;
 
@@ -13,13 +15,22 @@ public class HazelcastConfigRuntimeModify {
 	public static void main(String[] args) {
 		// 创建默认config对象
 		Config config = new Config();
-		// 获取NetworkConfig
+		
+		// 获取network元素<network></network>
 		NetworkConfig netConfig = config.getNetworkConfig();
 		System.out.println("Default port:" + netConfig.getPort());
-
-		// 设置NetworkConfig
+		
+		// 设置组网起始监听端口
 		netConfig.setPort(9701);
 		System.out.println("Customer port:" + netConfig.getPort());
+		// 获取join元素<join></join>
+		JoinConfig joinConfig = netConfig.getJoin();
+		// 获取multicast元素<multicast></multicast>
+		MulticastConfig multicastConfig = joinConfig.getMulticastConfig();
+		// 输出组播协议端口
+		System.out.println(multicastConfig.getMulticastPort());
+		// 禁用multicast协议
+		multicastConfig.setEnabled(false);
 		
 		// 初始化Hazelcast
 		Hazelcast.newHazelcastInstance(config);
